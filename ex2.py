af0383f3adea12ae411f7f8f65a193f90ac2dc62
Hangukkid <matthew.won@mail.utoronto.ca>
2020-09-21 16:38:52 -0400

    renaming ex2-1 to a general example from chapter 2. Added new feature to ex2 to allow more personalized user pages which include name of user.

--- ex2.py
+++ ex2.py
@@ -2,8 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
 if __name__ == '__main__':
     app.run(debug=True)
